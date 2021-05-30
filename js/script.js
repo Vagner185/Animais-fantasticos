26a1c3452b0bb85d04ee62a8128651946e02d060
Vagner <vagnerferreira185@gmail.com>
2021-05-30 11:59:16 -0300

    Adicionada function initModal ao js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimaçãoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
 
 initScrollSuave();
 initAnimaçãoScroll();
 initAccordion();
 initTabNav();
+initModal();
